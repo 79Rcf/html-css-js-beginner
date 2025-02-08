310d6b5f90a7094dd1d100671189c430ff570027
ulrich killian <ulrichkillian@gmail.com>
2025-02-08 16:19:07 +0100

    Initial commit - tracking all my code

---
+++ script.js
@@ -0,0 +1,9 @@
+function greet () {
+    console.log("hello, welcome to java script!");
+}
+greet();
+function greetuser(name) {
+    console.log("hello, welcome " + name + "!");
+}
+greetuser("john");
+greetuser("jane");
